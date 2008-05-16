b08cc734f160de260450c45b4b2ae62a70add09e
thvortex <thvortex@600054b2-aa4b-0410-823d-1969d9c2b2de>
2008-05-16 15:09:24 +0000

    A    tiled AM   tiled/gg2tiled.py Converter from our ASCII format to Tiled map editor XML format,
    AM   tiled/tiles.png
    We need a dummy tile number 0 in the image for the tile numbers in to 
    correctly correspond to the ASCII file.

---
+++ trunk/experiments/map_editors/tiled/gg2tiled.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import sys, struct, os, itertools, base64, gzip
+import cStringIO as sio
+
+def convert(filename):
+    # Load the ASCII map file; for now don't handle boundry data
+    tileset, tile_str, coll_str = open(filename).read().split('\n\n')
+    tiles = [s.split(' ') for s in tile_str.split('\n')]
+
+    width = len(tiles[0])
+    height = len(tiles)
+
+    # Create an in memory Gzip file to hold compressed layer data
+    gzipdata = sio.StringIO()
+    rawdata = gzip.GzipFile(None, "w", 9, gzipdata)
+
+    # Convert tile map layer into an array of 32bit integers
+    for row in tiles:
+        row = itertools.imap(int, row)
+        rawdata.write(struct.pack("<%di" % width, *row))
+
+    # Open the output file and generate XML
+    filename = os.path.splitext(filename)[0] + ".tmx"
+    outfile = open(filename, "w")
+
+    outfile.write("""\
+<?xml version="1.0" ?>
+<map version="0.99b" orientation="orthogonal" \
+width="%d" height="%d" tilewidth="16" tileheight="16">
+  <tileset firstgid="0" name="%s" tilewidth="16" tileheight="16">
+    <image source="%s"/>
+  </tileset>
+  <layer name="Background">
+    <data encoding="base64" compression="gzip">
+""" % (width, height, tileset, tileset))
+
+    # Base64 encode the compressed binary data before writing to XML
+    rawdata.close()
+    outfile.write(base64.b64encode(gzipdata.getvalue()))
+    gzipdata.close()
+
+    outfile.write("""
+    </data>
+  </layer>
+</map>
+""")
+
+    outfile.close()
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print "Usage: %s input.level" % sys.argv[0]
+        sys.exit()
+
+    convert(sys.argv[1])
