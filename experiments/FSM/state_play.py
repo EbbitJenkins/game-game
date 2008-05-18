120bcda787a93d4ccaf4edb72d96c7782ef39e86
phil.bordelon <phil.bordelon@600054b2-aa4b-0410-823d-1969d9c2b2de>
2008-05-18 16:02:55 +0000

    * /experiments/FSM/*:    De-tabbify and make UNIXy.

--- experiments/FSM/state_play.py
+++ experiments/FSM/state_play.py
@@ -3,19 +3,19 @@ import player
 import fsm
 
 class play:
-	"play"
-	def timer(self, dt):
-		next_state = self
-		if not (fsm.player.curr_state.__doc__ == "death" and fsm.player.lives < 1):
-			print "cycle %d" % (self.count, ) 
-			self.count = self.count + dt
-			fsm.player.do_timer(dt)
-		else:
-			next_state = state.death()
-		return next_state
-	def enter(self):
-		print "enter play"
-		self.count = 1
-		fsm.player = player.player(2, 5)
-	def leave(self):
-		print "leave play"
+    "play"
+    def timer(self, dt):
+        next_state = self
+        if not (fsm.player.curr_state.__doc__ == "death" and fsm.player.lives < 1):
+            print "cycle %d" % (self.count, ) 
+            self.count = self.count + dt
+            fsm.player.do_timer(dt)
+        else:
+            next_state = state.death()
+        return next_state
+    def enter(self):
+        print "enter play"
+        self.count = 1
+        fsm.player = player.player(2, 5)
+    def leave(self):
+        print "leave play"
