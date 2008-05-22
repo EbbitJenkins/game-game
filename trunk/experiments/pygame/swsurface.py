acee6ffeca8ee9c59c700ec730d7de6e3c1864e0
thvortex <thvortex@600054b2-aa4b-0410-823d-1969d9c2b2de>
2008-05-22 15:35:41 +0000

    M    pygame/swsurface.py User resizable window. Game logic and movement speed is independant of window size.

--- trunk/experiments/pygame/swsurface.py
+++ trunk/experiments/pygame/swsurface.py
@@ -2,10 +2,6 @@
 
 import sys, pygame, random
 
-# TODO: Separate the game logic from rendering better. The game logic should
-# only deal with 320x240 coordinates and speeds. Right before actual blitting
-# the rendering code should multiply all the coordinates by the display ratios.
-
 # Pygame 1.8.0 is missing the SDL_APPxxx bit flags!!!
 pygame.APPMOUSEFOCUS = 0x01
 pygame.APPINPUTFOCUS = 0x02
@@ -19,11 +15,11 @@ winsize = (640, 480)
 balls = 100
 fps = 60
 
-# The ratio between the display window size and the reference 320x240 resolution
-# is used for scaling all game images and movement speeds. For the most accurate
-# scaling, the window size should be an integer multiple of 320x240
-widthRatio = winsize[0] / 320.0
-heightRatio = winsize[1] / 240.0
+# All game logic, movement speed, and object locations are based on this
+# reference size of 320x240. During actual drawing, the cameara and sprite
+# coordinates are scaled to match the window size. This also defines the minimum
+# allowable window size the user can set.
+refsize = (320, 240)
 
 # The renderTicks variable accumulates the total number of milliseconds used to
 # render each frame. The frameCount is used to display renderTicks in the windows
@@ -49,8 +45,12 @@ def onEvent(event):
         sys.exit()
 
     # If the ESC key was hit, then also quit this shindig
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+    elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
         sys.exit()
+
+    # If user resized window, then re-scale all graphics
+    elif event.type == pygame.VIDEORESIZE:
+        onResize(event.size)
 
     # If the window looses/gains input focus, update hasInputFocus
     elif event.type == pygame.ACTIVEEVENT:
@@ -61,19 +61,63 @@ def onEvent(event):
 random.seed()
 pygame.init()
 
+# When the user resizes the main window, recreate the main display surface to
+# match new window size, and reload and rescale all graphics to the new size.
+def onResize(size):
+    global screen, bgsize, ratio
+    global tileImage, tileSize
+    global ballImage, ballSize
+    global bg
+
+    # Enforce the minimum window size defined by refsize
+    width = size[0] if size[0] > refsize[0] else refsize[0]
+    height = size[1] if size[1] > refsize[1] else refsize[1]
+
+    # Create 8bit color depth window display surface
+    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE, 8)
+    bgsize = screen.get_rect()
+
+    # Load the combined palette for all images into the screen surface.
+    # All further Surface.convert() calls on other surface images will
+    # remap the color indices to match the loaded palette as long as both
+    # palette's have identical RGB triples somewhere. Note that we don't call
+    # convert() on the palette image itself or we'd loose its color information.
+    screen.set_palette(pygame.image.load("palette.png").get_palette())
+
+    # The ratio between the display window size and the reference 320x240
+    # resolution is used for scaling all game images and movement speeds. For
+    # the most accurate scaling, the window size should be an integer multiple
+    # of 320x240.
+    ratio = float(bgsize.width) / refsize[0], float(bgsize.height) / refsize[1]
+
+    # Load all image resources
+    tileImage, tileSize = loadImage("tile.png")
+    ballImage, ballSize = loadImage("ball.png")
+
+    # Pre-render the background tile map
+    bg = initBackground()
+
+# Given a (x, y) tuple in the reference 320x240 coordinate space, return a new
+# tuple that is scaled to match the actual window size. For the most accurate
+# scaling, the window size should be an integer multiple of 320x240. This is
+# function is used to scale images, coordinates, and movement speeds.
+def scale((x,y)):
+    return int(x * ratio[0]), int(y * ratio[1])
+
 # Load a sprite/game image and pre-scale it to match the output display surface.
 # It turns out that the Surface.blit() routines are so optimized it's more
 # efficient to pre-scale and blit at the full resolution, even when blitting
 # overlapping areas, then it would be to blit at 320x240 to an intermediate
 # surface and then scale the entire display in realtime. This routine also
 # reindexes the images palette indices of the loaded image with
-# Surface.convert().
+# Surface.convert(). Also returns the size of the original image which is
+# used by the game logic to remain independant of the actual screen size.
 def loadImage(filename):
     image = pygame.image.load(filename).convert()
-    width, height = image.get_size()
+    size = image.get_size()
     
-    newwidth, newheight = int(width * widthRatio), int(height * heightRatio)
-    newimage = pygame.transform.scale(image, (newwidth, newheight))
+    newsize = scale(size)
+    newimage = pygame.transform.scale(image, newsize)
 
     # Enabling run length encoding of the color key transparency map on a
     # SWSURFACE will slightly increase the blit speed when using this surface
@@ -81,7 +125,7 @@ def loadImage(filename):
     # pronounced the speed up will be.
     newimage.set_colorkey(newimage.get_colorkey(), pygame.RLEACCEL)
     
-    return newimage
+    return newimage, size
 
 # Return random [x, y] list that can be used as a sprite/camera movement
 # speed. The returned x and y values are in the range of "-speed" to "speed",
@@ -91,39 +135,42 @@ def randomSpeed(speed):
     values += range(-1, -speed, -1)
     return [random.choice(values), random.choice(values)]
 
-# Return a new rectangle of the same size as "rect" but randomly positioned
-# within the screen area such that it still remains completely visible. The new
-# rect is suitable for use as a random starting location for sprites.
-def randomRect(rect):
-    x = random.randrange(size.width - rect.width)
-    y = random.randrange(size.height - rect.height)
-    return rect.move(x, y)
+# Return a new rectangle of the same size as (width, height) but randomly
+# positioned within the reference 320x240 area such that the rect still remains
+# completely visible. The new rect is suitable for use as a random starting
+# location for sprites.
+def randomRect((width, height)):
+    x = random.randrange(refsize[0] - width)
+    y = random.randrange(refsize[1] - height)
+    return pygame.Rect((x, y), (width, height))
 
 # It's more efficient to pre-render the entire scrolling tile map into an
-# intermediate surface and then display portions of it to the screen with a
-# single blit(), rather than redrawing the background from individual tiles
-# for each frame. Basically a memory vs speed tradeoff. Returns the newly
-# created surface.
+# intermediate surface (at the appropriate size for the current window)
+# and then display portions of it to the screen with a single blit(), rather
+# than redrawing the background from individual tiles for each frame. Basically
+# a memory vs speed tradeoff. Returns the newly created surface.
 def initBackground():
     # Calculate the number of tiles that must be drawn to fully cover the
     # window. To draw partial tile images at the edges of the window, we
-    # actually need one more tile since edges of the window are each displaying
-    # a partial tile image.
-    width, height = tile.get_rect().width, tile.get_rect().height
-    tileColumns = size.width / width + 1
-    tileRows = size.height / height + 1
+    # actually need one more tile in each dimension. Also since the window
+    # is currently user resizable and may not be a multiple of 2, we add an
+    # additional tile in each dimension just to make sure we don't wind up with
+    # blank pixels somewhere.
+    width, height = tileImage.get_size()
+    tileColumns = bgsize.width / width + 2
+    tileRows = bgsize.height / height + 2
 
     # Create the intermediate surface. Note that the call to convert() is
-    # necessary to initialize this surface's palette to match the palette in use
-    # by the screen surface.
-    bg = pygame.Surface((size.width + width, size.height + height)).convert()
+    # necessary to initialize this surface's palette to match the palette in
+    # use by the screen surface.
+    size = tileColumns * width, tileRows * height
+    bg = pygame.Surface(size).convert()
 
     # Draw the tiled background to the intermediate surface
-    origin = tile.get_rect()
     for row in xrange(tileRows):
         for col in xrange(tileColumns):
-            dest = origin.move(width * col, height * row)
-            bg.blit(tile, dest)
+            dest = width * col, height * row
+            bg.blit(tileImage, dest)
 
     return bg
 
@@ -134,9 +181,9 @@ def drawBackground(offset):
     # To draw partial tile images at the upper and left edges of the window, we
     # actually have to start drawing slightly outside of the window. Hence, the
     # subtraction of the tile width and height.
-    width, height = tile.get_rect().width, tile.get_rect().height
-    offset = (offset[0] - width, offset[0] - height)
-    screen.blit(bg, bg.get_rect().move(offset))
+    imageSize = tileImage.get_size()
+    offset = offset[0] - imageSize[0], offset[1] - imageSize[1]
+    screen.blit(bg, offset)
 
 # Update position of all the sprites and the scrolling background. Also perform
 # some rudimentary edge of window collision detection on each sprite
@@ -149,61 +196,49 @@ def onUpdate():
         speed = spriteSpeed[i]
         
         rect.move_ip(speed)
-        if rect.left < 0 or rect.right > size.width:
+        if rect.left < 0 or rect.right > refsize[0]:
             speed[0] = -speed[0]
-        if rect.top < 0 or rect.bottom > size.height:
+        if rect.top < 0 or rect.bottom > refsize[1]:
             speed[1] = -speed[1]
 
     # Update the camera's position using the current background scrolling speed.
     # By making the offset "wrap around" under modulo arithmetic, we can get a
     # continuously scrolling background pattern.
-    width, height = tile.get_rect().width, tile.get_rect().height
+    width, height = tileSize
     camera = (camera[0] + scroll[0]) % width, (camera[1] + scroll[1]) % height
 
-# Draw the scrolling background and the sprites at their current locations
+# Draw the scrolling background and the sprites at their current locations.
+# The camera and sprite locations are scaled up from their logical 320x240
+# coordinates to match the actual window size.
 def onDraw():
     # First blit the background so it appears underneath all sprites
-    drawBackground(camera)
+    drawBackground(scale(camera))
 
-    # Then blit every sprite at its current position
+    # Blit every sprite at its current position
     for i in xrange(balls):
-        screen.blit(ball, spriteRect[i])
+        dest = scale(spriteRect[i].topleft)
+        screen.blit(ballImage, dest)
     
 # The try..finally block ensures that pygame.quit() is always called to close the
 # main window. This is necessary when running under the IDLE Python GUI since the
 # Python interpreter is not actually shutdown when this piece of code finishes
 # or throws an exception.
 try:
+    pygame.display.set_caption("Pygame Speed Test")
 
     # The size of the real screen surface will be selectable by the user to
     # be a multiple of 320x240. We also want to set the video mode first so
     # that any subsequent surfaces we create can be convert()ed to the same
     # pixel format as the display for maximum blit speed.
-    pygame.display.set_caption("Pygame Speed Test")
-    screen = pygame.display.set_mode(winsize, 0, 8)
-    size = screen.get_rect()
-
-    # Load the combined palette for all images into the screen surface.
-    # All further Surface.convert() calls on other surface images will
-    # remap the color indices to match the loaded palette as long as both
-    # palette's have identical RGB triples somewhere. Note that we don't call
-    # convert() on the palette image itself or we'd loose its color information.
-    screen.set_palette(pygame.image.load("palette.png").get_palette())
-
-    # Load all image resources
-    tile = loadImage("tile.png")
-    ball = loadImage("ball.png")
-
-    # Pre-render the background tile map
-    bg = initBackground()
+    onResize(winsize)
 
     # Setup a random scrolling speed for the background
     scroll = randomSpeed(2)
     camera = (0, 0)
 
     # Create some sprites with random positions and movement speeds
-    spriteRect = [ randomRect(ball.get_rect()) for i in xrange(balls) ]
-    spriteSpeed = [ randomSpeed(10) for i in xrange(balls) ]
+    spriteRect = [ randomRect(ballSize) for i in xrange(balls) ]
+    spriteSpeed = [ randomSpeed(5) for i in xrange(balls) ]
 
     # Setup a timer object to maintain consistent frame rate
     timer = pygame.time.Clock()
@@ -248,7 +283,7 @@ try:
             average = float(renderTicks) / frameCount
             percent = (average * timer.get_fps()) / 10
             
-            data = (size.width, size.height, average, percent, timer.get_fps())
+            data = (bgsize.width, bgsize.height, average, percent, timer.get_fps())
             pygame.display.set_caption(caption % data)
             print(caption % data)
 
