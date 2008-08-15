04f9cbc3bba03afe4c17dee40fc4d6b7bc29f292
mgray16 <mgray16@600054b2-aa4b-0410-823d-1969d9c2b2de>
2008-08-15 02:28:17 +0000

    Integrated an event system into the sprite module. Moved to a frame-based timing system instead of a time-based one.
    em.py - Event Machine, as well as supporting classes.

--- experiments/FSM/state_load.py
+++ experiments/FSM/state_load.py
@@ -3,6 +3,7 @@ import engine
 import state
 import marek
 import spider
+import em
 
 #LOAD state is for loading a map, as well as everything involved with the map.  Goes to PLAY
 class STATE_load(state.State):
@@ -15,10 +16,24 @@ class STATE_load(state.State):
     def leave(self):
         print "leave load"
     def init(self):	    
+        #  This should all be done by the manifest parser
         globals.marek = marek.marek(120, 100, 2)    # initialize the marek object        
         increment = 580/int(globals.spider_number)
-        for i in range(20, 600, increment):
-            globals.spiders.append(spider.spider(i, 420))   # initialize spider object
+        for i in range(20, 600, increment):            
+            sprite = spider.spider(i, 420)          
+            images = [engine.load_image('spider.png')]
+            ani = em.Animation(images)            
+            event = em.SpriteEvent("onIdle", None, ani, 0, 0)
+            sprite.em.add(event)
+            event = em.SpriteEvent("onWalkLeft", None, ani, 0, 0)
+            sprite.em.add(event)
+            event = em.SpriteEvent("onWalkRight", None, ani, 0, 0)
+            sprite.em.add(event)
+            event = em.SpriteEvent("onJump", None, ani, 0, 0)
+            sprite.em.add(event)
+            event = em.SpriteEvent("onFall", None, ani, 0, 0)
+            sprite.em.add(event)            
+            globals.spiders.append(sprite)  
         globals.camera = engine.Camera(globals.map, globals.marek, globals.window.width, globals.window.height)
 		
     
